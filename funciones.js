8d34987d856c6733defa21b6b245de8e16772298
Lucas Lazzarotto <lucas.a.lazza@gmail.com>
2025-11-01 18:29:46 -0300

    Agrego archivo funciones.js

---
+++ funciones.js
@@ -0,0 +1,12 @@
+//Declaraciones
+//Aqui definir las variables globales
+let nombre = "Lucas";
+let edad = 23;
+
+//Funciones
+//Aqui definir las funciones
+function saludar(){
+    console.log("Hola Mundo");
+}
+
+
